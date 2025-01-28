558c00e701a03f6b227d829ebedc5ce9b80f01c1
Michael Hahmigery <michaelhahmigery@gmail.com>
2025-01-28 13:38:28 +0200

    Optimize volume change logic. Add top_level property to UI items.

--- scripts/settings_screen.gd
+++ scripts/settings_screen.gd
@@ -4,9 +4,12 @@ extends Panel
 @onready var sfx_volume_slider: HSlider = $SettingsBackground/SFXVolumeBox/SFXVolumeSlider
 @onready var music_volume_slider: HSlider = $SettingsBackground/MusicVolumeBox/MusicVolumeSlider
 
+var sfx_bus_index = AudioServer.get_bus_index("SFX")
+var music_bus_index = AudioServer.get_bus_index("Music")
+
 func _ready():
-	sfx_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX"))
-	music_volume_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music"))
+	sfx_volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_index))
+	music_volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus_index))
 	settings_button.connect("pressed", _close)
 	sfx_volume_slider.connect("value_changed", _on_sfx_volume_changed)
 	music_volume_slider.connect("value_changed", _on_music_volume_changed)
@@ -16,7 +19,7 @@ func _close():
 	queue_free()
 
 func _on_sfx_volume_changed(value: float):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), value)
+	AudioServer.set_bus_volume_db(sfx_bus_index, linear_to_db(value))
 
 func _on_music_volume_changed(value: float):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+	AudioServer.set_bus_volume_db(music_bus_index, linear_to_db(value))
